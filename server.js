c9267d5aa9a4253a2e835ccad8a8cd12f2629b04
megh0682 <shah.iyer@gmail.com>
2017-07-11 01:40:13 -0500

    trail and error

--- server.js
+++ server.js
@@ -48,8 +48,9 @@ db.once("open", function() {
 // -------------------------------------------------
 //context root
   app.get('/',(req,res)=>{
-
-    res.sendFile(path.join(__dirname, './build', 'index.html'));
+    
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
 
   });
 
@@ -78,7 +79,10 @@ db.once("open", function() {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'./build/index.html'));
+
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
+
 });
 
 // // This is the route we will send delete requests to delete the article by id
